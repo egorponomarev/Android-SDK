a694f5b65c9a4091b2d79bba15736e7ec23bcc5c
Martin Harkins <mharkins@grio.com>
2014-09-04 18:00:59 -0700

    Removing tracking of some deleted files

--- PlayerSDK/src/main/java/com/feedfm/android/fm.feed.android.playersdk/model/PlayerLibraryInfo.java
+++
@@ -1,9 +0,0 @@
-package fm.feed.android.playersdk.model;
-
-/**
- * Created by mharkins on 8/28/14.
- */
-public class PlayerLibraryInfo {
-    public String versionName;
-    public int versionCode;
-}
